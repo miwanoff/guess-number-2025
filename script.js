8faf6247395fc10d334e44fe596bb46a57b601cf
miwanoff <marina.iva.noffa@gmail.com>
2025-09-14 12:39:58 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const x = Math.floor(Math.random()*11);;
+console.log(x);
+
+while (true) {
+  let n = prompt("Input a number from 0 to 10 or any symbol for exit");
+  //   if (){
+  //     alert("Input a number from 0 to 10!");
+  //     continue;
+  //   }
+  if (isNaN(n)) {
+    alert("Bye!");
+    break;
+  }
+  // To DO: Add if (n < x) 
+  if (n > x) {
+    alert("Too big!");
+  }
+  else if (n == x) {
+    alert("You win!");
+    break;
+  }
+}
